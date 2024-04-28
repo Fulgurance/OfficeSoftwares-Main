9f2f5e871db878904bf5e159e9f2c77133328b5c
Fulgurance <zohran.londais@gmail.com>
2024-04-28 21:45:33 +0100

    Finishing to implement libreoffice installer

--- Libreoffice/7.6.0-r.3/7.6.0-r.3.cr
+++ Libreoffice/7.6.0-r.3/7.6.0-r.3.cr
@@ -3,22 +3,65 @@ class Target < ISM::Software
     def configure
         super
 
-        runScript("autogen.sh", path: buildDirectoryPath)
-        configureSource([   "--prefix=/usr",
-                            "--disable-static"],
-                            buildDirectoryPath)
+        runScript(      "autogen.sh",
+                    [   "--prefix=/usr",
+                        "--sysconfdir=/etc",
+                        "--with-vendor=#{Ism.systemName}",
+                        "--with-lang=\'en-GB\'",
+                        "--with-help",
+                        "--with-myspell-dicts",
+                        "--without-junit",
+                        "--without-system-dicts",
+                        "--disable-dconf",
+                        "--disable-odk",
+                        "--enable-release-build=yes",
+                        "--enable-python=system",
+                        "--with-jdk-home=/opt/jdk",
+                        "--with-system-clucene",
+                        "--with-system-curl",
+                        "--with-system-epoxy",
+                        "--with-system-expat",
+                        "--with-system-glm",
+                        "--with-system-gpgmepp",
+                        "--with-system-graphite",
+                        "--with-system-harfbuzz",
+                        "--with-system-icu",
+                        "--with-system-jpeg",
+                        "--with-system-lcms2",
+                        "--with-system-libatomic_ops",
+                        "--with-system-libpng",
+                        "--with-system-libxml",
+                        "--with-system-nss",
+                        "--with-system-odbc",
+                        "--with-system-openldap",
+                        "--with-system-openssl",
+                        "--with-system-poppler",
+                        "--with-system-postgresql",
+                        "--with-system-redland",
+                        "--with-system-libtiff",
+                        "--with-system-libwebp",
+                        "--with-system-zlib"],
+                        path: buildDirectoryPath)
     end
 
     def build
         super
 
-        makeSource(path: buildDirectoryPath)
+        makeSource(["build"], path: buildDirectoryPath)
     end
     
     def prepareInstallation
         super
 
-        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","install"],buildDirectoryPath)
+        makeSource(["DESTDIR=#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}","distro-pack-install"],buildDirectoryPath)
+    end
+
+    def install
+        super
+
+        if option("Desktop-File-Utils")
+            runUpdateDesktopDatabaseCommand
+        end
     end
 
 end
