bc5b456f39902e72ddcb9da66928d9d206e62412
Fulgurance <zohran.londais@gmail.com>
2024-06-26 14:16:58 +0100

    Improved libreoffice installer implementation

--- Libreoffice/7.6.0-rc.3/7.6.0-rc.3.cr
+++ Libreoffice/7.6.0-rc.3/7.6.0-rc.3.cr
@@ -4,43 +4,44 @@ class Target < ISM::Software
         super
 
         runFile(file:       "autogen.sh",
-                arguments:  "--prefix=/usr                              \
-                            --sysconfdir=/etc                           \
-                            --with-vendor=#{Ism.settings.systemName}    \
-                            --with-lang=\'en-GB\'                       \
-                            --with-help                                 \
-                            --with-myspell-dicts                        \
-                            --without-junit                             \
-                            --without-system-dicts                      \
-                            --disable-dconf                             \
-                            --disable-odk                               \
-                            --without-java                              \
-                            --enable-release-build=yes                  \
-                            --enable-python=system                      \
-                            --with-jdk-home=/opt/jdk                    \
-                            --with-system-clucene                       \
-                            --with-system-curl                          \
-                            --with-system-epoxy                         \
-                            --with-system-expat                         \
-                            --with-system-glm                           \
-                            --with-system-gpgmepp                       \
-                            --with-system-graphite                      \
-                            --with-system-harfbuzz                      \
-                            --with-system-icu                           \
-                            --with-system-jpeg                          \
-                            --with-system-lcms2                         \
-                            --with-system-libatomic_ops                 \
-                            --with-system-libpng                        \
-                            --with-system-libxml                        \
-                            --with-system-nss                           \
-                            --with-system-odbc                          \
-                            --with-system-openldap                      \
-                            --with-system-openssl                       \
-                            --with-system-poppler                       \
-                            --with-system-postgresql                    \
-                            --with-system-redland                       \
-                            --with-system-libtiff                       \
-                            --with-system-libwebp                       \
+                arguments:  "--prefix=/usr                                          \
+                            --sysconfdir=/etc                                       \
+                            --with-vendor=#{Ism.settings.systemName}                \
+                            --with-lang=\'en-GB\'                                   \
+                            --with-help                                             \
+                            --with-myspell-dicts                                    \
+                            --without-junit                                         \
+                            --without-system-dicts                                  \
+                            --disable-dconf                                         \
+                            --disable-odk                                           \
+                            --without-java                                          \
+                            --enable-release-build=yes                              \
+                            --enable-python=system                                  \
+                            --with-jdk-home=/opt/jdk                                \
+                            #{option("Cups") ? "--enable-cups" : "--disable-cups"}  \
+                            --with-system-clucene                                   \
+                            --with-system-curl                                      \
+                            --with-system-epoxy                                     \
+                            --with-system-expat                                     \
+                            --with-system-glm                                       \
+                            --with-system-gpgmepp                                   \
+                            --with-system-graphite                                  \
+                            --with-system-harfbuzz                                  \
+                            --with-system-icu                                       \
+                            --with-system-jpeg                                      \
+                            --with-system-lcms2                                     \
+                            --with-system-libatomic_ops                             \
+                            --with-system-libpng                                    \
+                            --with-system-libxml                                    \
+                            --with-system-nss                                       \
+                            --with-system-odbc                                      \
+                            --with-system-openldap                                  \
+                            --with-system-openssl                                   \
+                            --with-system-poppler                                   \
+                            --with-system-postgresql                                \
+                            --with-system-redland                                   \
+                            --with-system-libtiff                                   \
+                            --with-system-libwebp                                   \
                             --with-system-zlib",
                 path:       buildDirectoryPath)
     end
